aaff9429ca9d35d51d2be4b05542679801f40185
marchvps <marchvps2020@gmail.com>
2026-02-21 06:35:56 +0000

    sdm660-common: Regenerate makefiles to drop com.dsi.ant@1.0.so
    Removed from proprietary-files.txt because it's built from source in external/ant-wireless.

--- sdm660-common-vendor.mk
+++ sdm660-common-vendor.mk
@@ -88,7 +88,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/sdm660-common/proprietary/vendor/radio/qcril_database/upgrade/7_version_update_ecc_table.sql:$(TARGET_COPY_OUT_VENDOR)/radio/qcril_database/upgrade/7_version_update_ecc_table.sql
 
 PRODUCT_PACKAGES += \
-    com.dsi.ant@1.0_vendor \
     eglSubDriverAndroid \
     libEGL_adreno \
     libGLESv1_CM_adreno \
